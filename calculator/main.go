f50b356694881b8c2a925e26c4f503fb9a03f84f
lealclarissa <lealclarissa2@gmail.com>
2021-08-19 17:22:18 -0300

    change variables's names to English and update package's path

--- calculator/main.go
+++ calculator/main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 
-	"github.com/lealclarissa/go-practice/tree/main/calculator/media"
+	"github.com/lealclarissa/go-practice/tree/main/calculator/mean"
 )
 
 func main() {
 	fmt.Println("Insira a primeira nota: ")
-	var notaA float32
-	fmt.Scanln(&notaA)
+	var gradeA float32
+	fmt.Scanln(&gradeA)
 
 	fmt.Println("Insira a segunda nota: ")
-	var notaB float32
-	fmt.Scanln(&notaB)
+	var gradeB float32
+	fmt.Scanln(&gradeB)
 
 	fmt.Println("Insira a terceira nota: ")
-	var notaC float32
-	fmt.Scanln(&notaC)
+	var gradeC float32
+	fmt.Scanln(&gradeC)
 
-	var mean float32 = media.DigaMedia(notaA, notaB, notaC)
-	if mean >= 7 {
+	var myMean float32 = mean.GetMean(gradeA, gradeB, gradeC)
+	if myMean >= 7 {
 		fmt.Println("Parabéns, você foi aprovade!")
-	} else if mean < 5 {
+	} else if myMean < 5 {
 		fmt.Println("Sinto muito, você foi reprovade!")
 	} else {
 		fmt.Println("Você está em recuperação! Estude!")
