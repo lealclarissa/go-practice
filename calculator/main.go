d17b5bdbc4ba1368d9d553144aa5d442fc4f439d
lealclarissa <lealclarissa2@gmail.com>
2021-08-19 12:55:18 -0300

    create conditionals to return approval's statuses

--- calculator/main.go
+++ calculator/main.go
@@ -18,5 +18,13 @@ func main() {
 	fmt.Println("Insira a terceira nota: ")
 	var notaC float32
 	fmt.Scanln(&notaC)
-	media.DigaMedia(notaA, notaB, notaC)
+
+	var mean float32 = media.DigaMedia(notaA, notaB, notaC)
+	if mean >= 7 {
+		fmt.Println("Parabéns, você foi aprovade!")
+	} else if mean < 5 {
+		fmt.Println("Sinto muito, você foi reprovade!")
+	} else {
+		fmt.Println("Você está em recuperação! Estude!")
+	}
 }
