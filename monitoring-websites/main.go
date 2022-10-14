a4932548141f6b291ca202d6f187f2d1aba08e69
clarissa.leal <clarissa.leal@olaisaac.com.br>
2022-10-14 20:06:27 -0300

    chore: remove excessive print and duplicated error messages

--- monitoring-websites/main.go
+++ monitoring-websites/main.go
@@ -26,74 +26,70 @@ func main() {
 		case 1:
 			startMonitoring()
 		case 2:
-			fmt.Println("Exibindo Logs...")
-			fmt.Println("")
+			fmt.Printf("\nExibindo Logs...\n")
 			printLogs()
 		case 0:
-			fmt.Println("Saindo do programa...")
+			fmt.Printf("\nSaindo do programa...\n")
 			os.Exit(0)
 		default:
-			fmt.Println("Não conheço este comando")
+			fmt.Printf("\nNão conheço este comando\n")
 			os.Exit(-1)
 		}
 	}
 }
 
 func showIntroduction() {
-	version := 1.3
+	version := 1.4
 	var name string
 	fmt.Println("Olá, antes de iniciarmos, qual seu nome?")
 	fmt.Scan(&name)
-	fmt.Println("")
-	fmt.Println("Olá, ", name, "!", "Este programa está na versão", version)
-	fmt.Println("O que deseja fazer? Por favor, digite um dos números abaixo:")
-	fmt.Println("")
+
+	fmt.Printf("\nOlá, %v! Este programa está na versão %v\nO que deseja fazer? Por favor, digite um dos números abaixo:\n", name, version)
 }
 
 func showMenu() {
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
+	fmt.Println("1 - Iniciar Monitoramento\n2 - Exibir Logs \n0 - Sair do Programa")
 }
 
 func readCommand() int {
 	var commandRead int
 	fmt.Scan(&commandRead)
-	fmt.Println("O comando escolhido foi:", commandRead)
-	fmt.Println("")
+
+	fmt.Printf("O comando escolhido foi: %v", commandRead)
 
 	return commandRead
 }
 
 func startMonitoring() {
-	fmt.Println("Monitorando...")
-	fmt.Println("")
+	fmt.Printf("\nMonitorando...\n")
 
 	websites := readWebsitesFromFile()
 
 	for i := 0; i < monitoring; i++ {
 		for i, website := range websites {
-			fmt.Println("Testando site", i, ":", website)
+			fmt.Printf("\nTestando site %v: %v\n", i, website)
 			testWebsite(website)
 		}
 		time.Sleep(delay * time.Second)
-		fmt.Println("")
 	}
-	fmt.Println("")
+}
+
+func errorMessage(err error) {
+	fmt.Printf("Ocorreu um erro: %v", err)
 }
 
 func testWebsite(website string) {
 	resp, err := http.Get(website)
 
 	if err != nil {
-		fmt.Println("Ocorreu um erro: ", err)
+		errorMessage(err)
 	}
 
 	if resp.StatusCode == 200 {
-		fmt.Println("O site", website, "foi carregado com sucesso!")
+		fmt.Printf("O site %v foi carregado com sucesso!\n", website)
 		saveLog(website, true)
 	} else {
-		fmt.Println("O site", website, "está com problemas! Status Code:", resp.StatusCode)
+		fmt.Printf("O site: %v está com problemas! Status Code: %v\n", website, resp.StatusCode)
 		saveLog(website, false)
 	}
 }
@@ -103,7 +99,7 @@ func readWebsitesFromFile() []string {
 
 	file, err := os.Open("websites.txt")
 	if err != nil {
-		fmt.Println("Ocorreu um erro: ", err)
+		errorMessage(err)
 	}
 
 	reader := bufio.NewReader(file)
@@ -126,9 +122,8 @@ func readWebsitesFromFile() []string {
 
 func saveLog(site string, status bool) {
 	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-
 	if err != nil {
-		fmt.Println("Ocorreu um erro: ", err)
+		errorMessage(err)
 	}
 
 	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "- está online: " + strconv.FormatBool(status) + "\n")
@@ -138,9 +133,8 @@ func saveLog(site string, status bool) {
 
 func printLogs() {
 	file, err := ioutil.ReadFile("log.txt")
-
 	if err != nil {
-		fmt.Println("Ocorreu um erro: ", err)
+		errorMessage(err)
 	}
 
 	fmt.Println(string(file))
